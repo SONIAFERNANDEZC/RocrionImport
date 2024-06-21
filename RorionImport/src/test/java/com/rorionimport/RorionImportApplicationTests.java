43a4bad0dd12a42e7918f5f95ca4828dfd9210e8
SONIAFERNANDEZC <fernandezsonia82@gmail.com>
2024-06-21 13:24:56 +0200

    Subiendo el principio con la arquitectura

---
+++ RorionImport/src/test/java/com/rorionimport/RorionImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rorionimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RorionImportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
